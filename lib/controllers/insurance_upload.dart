e4583c2c31db9f8527085c403288458c15a82e77
ank809 <ankita51523@gmail.com>
2023-10-31 23:32:30 +0530

    image is being fetched after getting verified

--- lib/controllers/insurance_upload.dart
+++ lib/controllers/insurance_upload.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:hospital_app/model/data.dart';
@@ -18,4 +19,27 @@ class Update_Insurance{
     }
   }
 
-}
+  // fetching the data
+  static Stream<QuerySnapshot> getdata(){
+    return FirebaseFirestore.instance.collection('insurance_company_data').snapshots();
+  }
+
+  // update the data
+  static void update_data(String cardno) async {
+    CollectionReference reference= FirebaseFirestore.instance.collection('insurance_company_data');
+    // get the data of all where the condition is being satisfied
+    QuerySnapshot snapshot = await reference.where('card_no', isEqualTo: cardno).get();
+    if(snapshot.docs.isNotEmpty) {
+        DocumentReference ref = snapshot.docs[0].reference;
+        await ref.update({'isVerified' : true}).then((value) {
+          Get.snackbar('Verified', 'Image Verified');
+        });
+    } 
+    else {
+      log('Document Empty');
+    }
+  }
+  static deletedata(String documentID){
+    FirebaseFirestore.instance.collection('insurance_company_data').doc(documentID).delete();
+  }
+  }
--- lib/view/screens/company_screen.dart
+++ lib/view/screens/company_screen.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hospital_app/controllers/insurance_upload.dart';
+import 'package:hospital_app/controllers/patient_details.dart';
+import 'package:hospital_app/view/screens/upload_detail.dart';
+
 class CompanyScreen extends StatefulWidget {
   const CompanyScreen({super.key});
 
@@ -9,11 +15,66 @@ class CompanyScreen extends StatefulWidget {
 class _CompanyScreenState extends State<CompanyScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        color: Colors.blue,
+        color: Colors.red,
+        child: StreamBuilder<QuerySnapshot>(
+          stream: Update_Insurance.getdata(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return const Center(
+                child: Text('No data found',
+                    style: TextStyle(color: Colors.white)),
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(
+                  child: Text('Error: ${snapshot.error}',
+                      style: TextStyle(color: Colors.white)));
+            }
+            List<DocumentSnapshot> document = snapshot.data!.docs;
+            return ListView.builder(
+                itemCount: document.length, itemBuilder: (context, index) {
+                   final item = document[index].data() as Map<String, dynamic>;
+                   return Card(
+                    child: Column(
+                      children: [
+                        Row(children: [
+                          const Text('Patient\'s Card no:'),
+                          Text(item['card_no']),
+                        ],),
+                        Container(
+                          child: Image.network(item['imageURL'],
+                          fit: BoxFit.contain,),
+                        ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ElevatedButton(onPressed: (){
+                                Navigator.pop(context);
+                              }, child: Text('Decline')),
+                              ElevatedButton(onPressed: (){
+                                Update_Insurance.update_data(item['card_no']);
+                                PatientDetails.update_details(item['card_no'], item['imageURL']);
+                                Update_Insurance.deletedata(document[index].id);
+
+                              }, child: Text('Accept'))
+                            ],
+                          ),
+
+                      ],
+                    ),
+                   );
+                });
+          },
+        ),
       ),
     );
   }
